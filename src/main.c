184c96980b565538a2de45f09989b1381c803726
Michele Carenza <michele.carenza@proton.me>
2024-07-16 17:40:22 +0200

    print to uart

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
-
+#include <stddef.h>
 /* ---------------------------------------------------- */
 /* --------------- DEFINES ---------------------------- */
 /* ---------------------------------------------------- */
@@ -15,9 +15,13 @@
 
 #define GPIO(bank)        (( struct gpio * )    ( 0x48000000 + (0x0400 * (bank))))
 #define RCC               (( struct rcc * )       0x58000000 )
+#define USART2            (( struct uart * )      0x40004400 )
+#define LPUART1           (( struct uart * )      0x40008000 )
+#define USART1            (( struct uart * )      0x40013800 )
 #define SYSTICK           (( struct systick * )   0xe000e010 )
 
 
+#define FREQ              4000000    // Cpu frequency, 4Mhz
 
 
 
@@ -88,6 +92,21 @@ struct rcc {
     volatile uint32_t CSR;
 };
 
+struct uart {
+    volatile uint32_t CR1;
+    volatile uint32_t CR2;
+    volatile uint32_t CR3;
+    volatile uint32_t BRR;
+    volatile uint32_t GTPR;
+    volatile uint32_t RTOR;
+    volatile uint32_t RQR;
+    volatile uint32_t ISR;
+    volatile uint32_t ICR;
+    volatile uint32_t RDR;
+    volatile uint32_t TDR;
+    volatile uint32_t PRESC;
+};
+
 struct systick {
 	volatile uint32_t CTRL;
 	volatile uint32_t LOAD;
@@ -120,14 +139,24 @@ enum {
 
 static volatile uint32_t s_ticks;
 
+static inline void spin(volatile uint32_t count) {
+    while (count--) (void) 0;
+}
 
 
-
-static void gpio_set_mode(uint16_t pin, uint8_t mode) {
+static inline void gpio_set_mode(uint16_t pin, uint8_t mode) {
     struct gpio* gpio = GPIO(PINBANK(pin));
     uint8_t n = PINNO(pin);
+    RCC->AHB2ENR |= BIT(PINBANK(pin));
     gpio->MODER &= ~( 0b11 << ( n * 2 ) );
     gpio->MODER |= ( mode << ( n * 2 ) );
+}
+
+static inline void gpio_set_af(uint16_t pin, uint8_t af_num) {
+    struct gpio* gpio = GPIO(PINBANK(pin));
+    uint8_t n = PINNO(pin);
+    gpio->AFR[ pin / 8 ] &= ~( 0b1111 << ( ( n % 8 ) * 4 ) );
+    gpio->AFR[ pin / 8 ] |= af_num << ( ( n % 8 ) * 4 );
 }
 
 static inline void gpio_write(uint16_t pin, bool val) {
@@ -135,9 +164,47 @@ static inline void gpio_write(uint16_t pin, bool val) {
     gpio->BSRR = ( 1U << PINNO(pin) ) << ( val ? 0 : 16 );
 }
 
-static inline void spin(volatile uint32_t count) {
-    while (count--) (void) 0;
+
+
+static inline void uart_init(struct uart* uart, unsigned long baud) {
+    uint8_t af = 8;
+    uint16_t rx = 0, tx = 0;
+
+    if (uart == LPUART1) {
+        RCC->APB1ENR2 |= BIT(0);
+        tx = PIN('A', 2);
+        rx = PIN('A', 3);
+    }
+
+    gpio_set_mode(tx, GPIO_MODE_ALTERNATE_FUNCTION);
+    gpio_set_af(tx, af);
+
+    gpio_set_mode(rx, GPIO_MODE_ALTERNATE_FUNCTION);
+    gpio_set_af(rx, af);
+
+    uart->CR1 = 0;    // reset this uart
+    uart->BRR = ( ( 256 * FREQ ) ) / baud;
+    uart->CR1 = BIT(3) | BIT(2) | BIT(0);    // set TE, RE, UE
 }
+
+static inline int uart_read_ready(struct uart* uart) {
+    return uart->ISR & BIT(5);    // If RXNE bit is set, data is ready
+}
+
+static inline uint8_t uart_read_byte(struct uart* uart) {
+    return (uint8_t) uart->RDR & 0xff;
+}
+
+static inline void uart_write_byte(struct uart *uart, uint8_t byte) {
+  uart->TDR = byte;
+  while ((uart->ISR & BIT(7)) == 0) spin(1);
+}
+
+static inline void uart_write_buf(struct uart *uart, char *buf, size_t len) {
+  while (len-- > 0) uart_write_byte(uart, *(uint8_t *) buf++);
+}
+
+
 
 static inline void systick_init(uint32_t ticks) {
     if ((ticks - 1) > 0xffffff) return;
@@ -167,17 +234,20 @@ bool timer_expired(uint32_t *t, uint32_t prd, uint32_t now) {
 
 int main(void) {
     uint16_t led = PIN('B', 15);
-    RCC->AHB2ENR |= BIT(PINBANK(led));
     gpio_set_mode(led, GPIO_MODE_OUTPUT);
     systick_init(4000000 / 1000);
 
     uint32_t timer, period = 500;    // Declare timer and 500ms period
+
+    uart_init(LPUART1, 115200);
 
     for (;;) {
         if (timer_expired(&timer, period, s_ticks)) {
             static bool on;
             gpio_write(led, on);
             on = !on;
+
+            uart_write_buf(LPUART1, "hi\r\n", 4);
         }
     };
 
